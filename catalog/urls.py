88976462eb01ebb487c3bb0a54935d2e6b447588
Ilya Evlanov <ilya.evlanov@gmail.com>
2023-07-10 19:19:47 +0300

    modified catalog/urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from catalog.views import contacts, home
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('', contacts, name='contacts')
+    path('', home, name='index'),
+    path('home/', home, name='home'),
+    path('contacts/', contacts, name='contacts')
 ]
