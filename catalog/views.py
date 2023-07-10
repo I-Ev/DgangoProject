f5c1e5810c5cf9c270874a02b7f7fca2e92c8001
Ilya Evlanov <ilya.evlanov@gmail.com>
2023-07-10 12:13:29 +0300

    fixed contacts.html and views.py

--- catalog/views.py
+++ catalog/views.py
@@ -5,4 +5,9 @@ def home(request):
     return render(request, 'catalog/home.html')
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        print(f'name: {name}, phone: {phone}, message: {message}')
     return render(request, 'catalog/contacts.html')
