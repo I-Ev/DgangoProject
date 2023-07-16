eeeb61d711d037970b220abcc9090c1d5716acd6
Ilya Evlanov <ilya.evlanov@gmail.com>
2023-07-16 19:40:08 +0300

    Task 5 of HomeWork 20.1

--- catalog/models.py
+++ catalog/models.py
@@ -3,11 +3,24 @@ from django.db import models
 NULLABLE = {'blank': True, 'null': True}
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
 class Product(models.Model):
     name = models.CharField(max_length=200, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание')
     image = models.ImageField(upload_to='products/', **NULLABLE, verbose_name='Изображение')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
     price = models.FloatField(verbose_name='Цена')
     date_created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     date_last_modified = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
@@ -18,17 +31,3 @@ class Product(models.Model):
     class Meta:
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=200, verbose_name='Наименование')
-    description = models.TextField(verbose_name='Описание')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
-
-
-    def __str__(self):
-        return f'{self.name}'
-
-    class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
