6ec1323a89b3ca2995bfab309a1df68759cda5e3
Ilya Evlanov <ilya.evlanov@gmail.com>
2023-07-16 11:28:07 +0300

    Task 2-4 of HomeWork 20.1

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='products/', **NULLABLE, verbose_name='Изображение')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
+    price = models.FloatField(verbose_name='Цена')
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    date_last_modified = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name} ({self.category})'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
