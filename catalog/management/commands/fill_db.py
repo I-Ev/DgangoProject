403989a89850194d022ed5ac924293683a3f64b0
Ilya Evlanov <ilya.evlanov@gmail.com>
2023-07-17 22:14:46 +0300

    modified fill_db.py

--- catalog/management/commands/fill_db.py
+++ catalog/management/commands/fill_db.py
@@ -10,16 +10,16 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         Category.objects.all().delete()
         Product.objects.all().delete()
-        with open('data.json', 'r', encoding='UTF-8') as f:
-            data = json.load(f, encoding='utf-8')
+        with open('data.json', 'r', encoding='utf-16') as f:
+            data = json.load(f)
 
             categories_for_create=[]
             products_for_create=[]
             for i in data:
                 if i['model'] == 'catalog.category':
-                    categories_for_create.append(Category(**i))
+                    categories_for_create.append(Category(**i['fields']))
                 elif i['model'] == 'catalog.product':
-                    products_for_create.append(Product(**i))
+                    products_for_create.append(Product(**i['fields']))
 
             Category.objects.bulk_create(categories_for_create)
             Product.objects.bulk_create(products_for_create)
