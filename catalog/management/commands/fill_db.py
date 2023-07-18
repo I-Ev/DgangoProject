fd6fdfedfd6de271349fa7e4133ef8a5d38013d3
Ilya Evlanov <ilya.evlanov@gmail.com>
2023-07-18 22:02:01 +0300

    modified fill_db.py

--- catalog/management/commands/fill_db.py
+++ catalog/management/commands/fill_db.py
@@ -1,3 +1,30 @@
+# import json
+#
+# from django.core.management import BaseCommand
+#
+# from catalog.models import Product, Category
+#
+#
+# class Command(BaseCommand):
+#
+#     def handle(self, *args, **options):
+#         Category.objects.all().delete()
+#         Product.objects.all().delete()
+#         with open('data.json', 'r', encoding='utf-16') as f:
+#             data = json.load(f)
+#
+#             categories_for_create=[]
+#             products_for_create=[]
+#             for i in data:
+#                 if i['model'] == 'catalog.category':
+#                     categories_for_create.append(Category(**i['fields']))
+#                 elif i['model'] == 'catalog.product':
+#                     products_for_create.append(Product(**i['fields']))
+#
+#             Category.objects.bulk_create(categories_for_create)
+#             Product.objects.bulk_create(products_for_create)
+
+
 import json
 
 from django.core.management import BaseCommand
@@ -10,16 +37,32 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         Category.objects.all().delete()
         Product.objects.all().delete()
+
         with open('data.json', 'r', encoding='utf-16') as f:
             data = json.load(f)
 
-            categories_for_create=[]
-            products_for_create=[]
-            for i in data:
-                if i['model'] == 'catalog.category':
-                    categories_for_create.append(Category(**i['fields']))
-                elif i['model'] == 'catalog.product':
-                    products_for_create.append(Product(**i['fields']))
+        categories_for_create = []
+        products_for_create = []
 
-            Category.objects.bulk_create(categories_for_create)
-            Product.objects.bulk_create(products_for_create)
+        for i in data:
+            if i['model'] == 'catalog.category':
+                category = Category(
+                    pk=i['pk'],
+                    name=i['fields']['name'],
+                    description=i['fields']['description'],
+                    created_at=i['fields']['created_at']
+                )
+                categories_for_create.append(category)
+            elif i['model'] == 'catalog.product':
+                product = Product(
+                    pk=i['pk'],
+                    name=i['fields']['name'],
+                    description=i['fields']['description'],
+                    image=i['fields']['image'],
+                    category_id=i['fields']['category'],
+                    price=i['fields']['price']
+                )
+                products_for_create.append(product)
+
+        Category.objects.bulk_create(categories_for_create)
+        Product.objects.bulk_create(products_for_create)
