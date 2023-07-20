ea2bcbbcfcd9a03d64a98bb7109388083a454d86
Ilya Evlanov <ilya.evlanov@gmail.com>
2023-07-20 15:59:56 +0300

    part3 of homework 20_2

--- catalog/templatetags/my_tags.py
+++ catalog/templatetags/my_tags.py
@@ -4,6 +4,11 @@ register = Library()
 
 @register.filter
 def mediapath(path):
-    return "/media/" + path
+    path_str = str(path)
+    return "/media/" + path_str
 
 
+@register.simple_tag
+def mediapath(path):
+    path_str = str(path)
+    return "/media/" + path_str
