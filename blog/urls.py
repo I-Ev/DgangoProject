7a73b79d3521931518e5e69d7e193de55921064c
Ilya Evlanov <ilya.evlanov@gmail.com>
2023-07-24 14:57:47 +0300

    CRUD for model BlogEntry

--- blog/urls.py
+++ blog/urls.py
@@ -3,16 +3,16 @@ from django.conf.urls.static import static
 from django.urls import path
 
 from blog.apps import BlogConfig
-from blog.views import BlogEntryCreateView, BlogEntryListView, BlogEntryDetailView
+from blog.views import BlogEntryCreateView, BlogEntryListView, BlogEntryDetailView, BlogEntryUpdateView, \
+    BlogEntryDeleteView
 
 app_name = BlogConfig.name
 
 urlpatterns = [
     path('create/', BlogEntryCreateView.as_view(), name='create'),
     path('', BlogEntryListView.as_view(), name='list'),
-    # path('<int:pk>/', BlogEntryListView.as_view(), name='detail'),
-    # path('edit/<int:pk>', ..., name='edit'),
+    path('edit/<int:pk>', BlogEntryUpdateView.as_view(), name='edit'),
     path('view/<int:pk>', BlogEntryDetailView.as_view(), name='detail'),
-    # path('delete/<int:pk>/', ..., name='delete'),
+    path('delete/<int:pk>/', BlogEntryDeleteView.as_view(), name='delete'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView, DetailView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 
 from blog.models import BlogEntry
 
@@ -10,8 +10,22 @@ class BlogEntryCreateView(CreateView):
     fields = ['title', 'body', 'preview']
     success_url = reverse_lazy('blog:list')
 
+
 class BlogEntryListView(ListView):
     model = BlogEntry
 
+
 class BlogEntryDetailView(DetailView):
     model = BlogEntry
+
+
+class BlogEntryUpdateView(UpdateView):
+    model = BlogEntry
+    fields = ['title', 'body', 'preview']
+    success_url = reverse_lazy('blog:list')
+
+
+class BlogEntryDeleteView(DeleteView):
+    model = BlogEntry
+    success_url = reverse_lazy('blog:list')
+
