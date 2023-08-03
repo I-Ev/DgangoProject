10d450f3da6b1441fb10326551ef95592706fbcf
Ilya Evlanov <ilya.evlanov@gmail.com>
2023-08-03 18:03:33 +0300

    стилицация форм через init

--- catalog/forms.py
+++ catalog/forms.py
@@ -3,7 +3,16 @@ from django import forms
 from catalog.models import Product
 
 
-class ProductForm(forms.ModelForm):
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            # field.help_text = 'help text'
+
+
+class ProductForm(StyleFormMixin, forms.ModelForm):
 
     stop_words = ["казино", "криптовалюта", "крипта", "биржа", "дешево", "бесплатно", "обман", "полиция", "радар"]
 
@@ -12,6 +21,9 @@ class ProductForm(forms.ModelForm):
         fields = '__all__'
         # fields = ('name', 'description', 'price', 'category')
         # exclude = ('id', 'date_created', 'date_updated', 'is_active')
+
+
+
 
     def clean_name(self, words=stop_words):
         clean_data = self.cleaned_data['name'].lower()
@@ -32,7 +44,7 @@ class ProductForm(forms.ModelForm):
         return clean_data
 
 
-class VersionForm(forms.ModelForm):
+class VersionForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
