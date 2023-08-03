8772d3df9cb1a31914a22dc6624a9dfc2006f799
Ilya Evlanov <ilya.evlanov@gmail.com>
2023-08-03 18:12:06 +0300

    добавление версии на карточки

--- catalog/templatetags/my_tags.py
+++ catalog/templatetags/my_tags.py
@@ -1,5 +1,6 @@
 from django.template import Library
 
+from catalog.models import Product, Version
 from config import settings
 
 register = Library()
@@ -16,3 +17,15 @@ def mediapath(value):
 def mediapath(path):
     path_str = str(path)
     return "/media/" + path_str
+
+
+@register.simple_tag
+@register.simple_tag
+def get_current_version(product_id):
+    try:
+        product = Product.objects.get(id=product_id)
+        current_version = product.version_set.get(is_actual_version=True)
+        if current_version.nomer is not None:
+            return f"v.{current_version.nomer}"
+    except:
+        return None
